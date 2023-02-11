6b51949616890fc023ec5a6678ba17ce2599b578
Mari Flåm <tabasco_world@hotmail.com>
2023-02-11 22:15:26 +0100

    add tests for the different mathematical operations the code performs

--- exercise1-uglycode/src/test/java/no/nerdschool/uglycode/healthCheckTest.java
+++ exercise1-uglycode/src/test/java/no/nerdschool/uglycode/healthCheckTest.java
@@ -5,12 +5,33 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import no.nerdschool.uglycode.HealthCheck;
 public class healthCheckTest {
-
     @Test
-    public void healthCheckTest() {
-        //Do stuff here
-        //assertEquals("hat", HealthCheck.cmp("1 2 +"));
-        //assertEquals(3, HealthCheck.cmp("1 2 +"));
-        assertEquals(30-15, HealthCheck.cmp("15 30 -"));
+    public void checkIfNotString() {
+        assertEquals(0, HealthCheck.cmp(1234));
+
+    }
+    //@Test
+    //public void checkIfNotNumbers() {
+       // assertEquals((Class String, HealthCheck.cmp("1 2 +".class)); //write one that checks type
+    //}
+    @Test
+    public void checkIfAddition() {
+        assertEquals(3, HealthCheck.cmp("1 2 +"));
+    }
+    @Test
+    public void checkIfSubtraction() {
+        assertEquals(-1, HealthCheck.cmp("1 2 -"));
+    }
+    @Test
+    public void checkIfMultiplication() {
+        assertEquals(2, HealthCheck.cmp("1 2 *"));
+    }
+    @Test
+    public void checkIfDivision() {
+        assertEquals(3, HealthCheck.cmp("6 2 /"));
+    }
+    @Test
+    public void checkIfOtherDivides() {
+        assertEquals(3, HealthCheck.cmp("6 2 7"));
     }
 }
