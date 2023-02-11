44f88ab8320edca651f08c67c07348f6d88f3665
Mari Flåm <tabasco_world@hotmail.com>
2023-02-11 22:33:15 +0100

    change the name of the main string to something more descriptive

--- exercise1-uglycode/src/main/java/no/nerdschool/uglycode/HealthCheck.java
+++ exercise1-uglycode/src/main/java/no/nerdschool/uglycode/HealthCheck.java
@@ -2,23 +2,24 @@ package no.nerdschool.uglycode;
 
 public class HealthCheck {
     public static int skipper = 1;
-    public static String c;
+    public static String inputString;
     public static int compute(Object i) {
         {
             if (i instanceof String) {
-                c = (String) i;
+                inputString = (String) i;
             } else return 0;
         }
-        int x = Integer.parseInt(c.substring(0, 1 + c.indexOf(' ') - skipper));
-        int jeff = Integer.parseInt(c.substring(skipper + c.indexOf(' '),
-                c.indexOf(' ', c.indexOf(' ') + 1)));
-        if (c.substring(c.indexOf(' ', 1 + c.indexOf(' ')) + skipper, c.length()).equals("+")) return x + jeff;
+        //String[] stringToList = inputString.split("_");
+        int x = Integer.parseInt(inputString.substring(0, 1 + inputString.indexOf(' ') - skipper));
+        int jeff = Integer.parseInt(inputString.substring(skipper + inputString.indexOf(' '),
+                inputString.indexOf(' ', inputString.indexOf(' ') + 1)));
+        if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper, inputString.length()).equals("+")) return x + jeff;
 
-        if (c.substring(c.indexOf(' ', 1 + c.indexOf(' ')) + skipper, c.length()).equals("-")) {
+        if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper, inputString.length()).equals("-")) {
             return -jeff - (-x); //x-jeff isteden?
         }
         {
-            if (c.substring(c.indexOf(' ', 1 + c.indexOf(' ')) + skipper).equals("*")) return x * jeff;
+            if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper).equals("*")) return x * jeff;
             else return jeff == 0 ? 0 :
                     (int) ((skipper / (double) jeff) * x);
         }
@@ -26,4 +27,3 @@ public class HealthCheck {
 
 
     }
-}
