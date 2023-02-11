3a86adecdb93cc8033c914831d5a0ab8b9fa8112
Mari Flåm <tabasco_world@hotmail.com>
2023-02-11 22:27:33 +0100

    changed name of test til to comply with class naming standards

--- exercise1-uglycode/src/test/java/no/nerdschool/uglycode/HealthCheckTest.java
+++ exercise1-uglycode/src/test/java/no/nerdschool/uglycode/HealthCheckTest.java
@@ -3,35 +3,34 @@ package no.nerdschool.uglycode;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import no.nerdschool.uglycode.HealthCheck;
-public class healthCheckTest {
+public class HealthCheckTest {
     @Test
     public void checkIfNotString() {
-        assertEquals(0, HealthCheck.cmp(1234));
+        assertEquals(0, HealthCheck.compute(1234));
 
     }
     //@Test
     //public void checkIfNotNumbers() {
-       // assertEquals((Class String, HealthCheck.cmp("1 2 +".class)); //write one that checks type
+       // assertEquals((Class String, HealthCheck.compute("1 2 +".class)); //write one that checks type
     //}
     @Test
     public void checkIfAddition() {
-        assertEquals(3, HealthCheck.cmp("1 2 +"));
+        assertEquals(3, HealthCheck.compute("1 2 +"));
     }
     @Test
     public void checkIfSubtraction() {
-        assertEquals(-1, HealthCheck.cmp("1 2 -"));
+        assertEquals(-1, HealthCheck.compute("1 2 -"));
     }
     @Test
     public void checkIfMultiplication() {
-        assertEquals(2, HealthCheck.cmp("1 2 *"));
+        assertEquals(2, HealthCheck.compute("1 2 *"));
     }
     @Test
     public void checkIfDivision() {
-        assertEquals(3, HealthCheck.cmp("6 2 /"));
+        assertEquals(3, HealthCheck.compute("6 2 /"));
     }
     @Test
     public void checkIfOtherDivides() {
-        assertEquals(3, HealthCheck.cmp("6 2 7"));
+        assertEquals(3, HealthCheck.compute("6 2 7"));
     }
 }
