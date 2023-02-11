26de519450221df04f06975c91b926a7f3ffba51
Mari Flåm <tabasco_world@hotmail.com>
2023-02-11 23:12:04 +0100

    rewrite the previous method with the new, easier to read if-checks

--- exercise1-uglycode/src/main/java/no/nerdschool/uglycode/HealthCheck.java
+++ exercise1-uglycode/src/main/java/no/nerdschool/uglycode/HealthCheck.java
@@ -3,29 +3,35 @@ package no.nerdschool.uglycode;
 public class HealthCheck {
     public static int skipper = 1;
     public static String inputString;
-    public static int compute(Object i) {
+    public static int compute(Object input) {
         {
-            if (i instanceof String) {
-                inputString = (String) i;
+            if (input instanceof String) {
+                inputString = (String) input;
             } else return 0;
         }
+        //need a test to see if the array does indeed have 3 indecies?
         //transform the input string into a list of strings, split on spaces
-        String[] listOfStrings = inputString.split("_");
-        int firstNumber = Integer.parseIntlistOfStrings[0]
-        int firstNumber = Integer.parseInt(inputString.substring(0, 1 + inputString.indexOf(' ') - skipper));
-        int secondNumber = Integer.parseInt(inputString.substring(skipper + inputString.indexOf(' '),
-                inputString.indexOf(' ', inputString.indexOf(' ') + 1)));
-        if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper, inputString.length()).equals("+")) return firstNumber + secondNumber;
+        String[] listOfStrings = inputString.split(" ");
 
-        if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper, inputString.length()).equals("-")) {
-            return -secondNumber - (-firstNumber); //firstNumber-secondNumber isteden?
+        //the first number in the list is assigned to the variable firstNumber
+        int firstNumber = Integer.parseInt(listOfStrings[0]);
+        int secondNumber = Integer.parseInt(listOfStrings[1]);
+        String operator = listOfStrings[2];
+
+        if (operator.equals("+")){
+            return firstNumber + secondNumber;
         }
-        {
-            if (inputString.substring(inputString.indexOf(' ', 1 + inputString.indexOf(' ')) + skipper).equals("*")) return firstNumber * secondNumber;
-            else return secondNumber == 0 ? 0 :
+        if (operator.equals("-")) {
+            return firstNumber-secondNumber;
+        }
+        if (operator.equals("*")) {
+            return firstNumber * secondNumber;
+        }
+        //if the second number is 0 it returns 0
+        // else it returns the second number divided by the first
+        else {
+            return secondNumber == 0 ? 0 :
                     (int) ((skipper / (double) secondNumber) * firstNumber);
         }
     }
-
-
     }
