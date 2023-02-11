b4f6cdbada4e15d7a99bc997cb498d44f9acbe9f
Mari Flåm <tabasco_world@hotmail.com>
2023-02-11 23:43:32 +0100

    add test to check if the string is not on the expected form

--- exercise1-uglycode/src/test/java/no/nerdschool/uglycode/HealthCheckTest.java
+++ exercise1-uglycode/src/test/java/no/nerdschool/uglycode/HealthCheckTest.java
@@ -1,40 +1,45 @@
 package no.nerdschool.uglycode;
 
 import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.*;
 public class HealthCheckTest {
     @Test
     public void checkIfNotString() {
-        assertEquals(0, HealthCheck.compute(1234));
-
+        assertEquals(0, HealthCheck.cmp(1234));
     }
     //@Test
     //public void checkIfNotNumbers() {
-       // assertEquals((Class String, HealthCheck.compute("1 2 +".class)); //write one that checks type
+       // assertEquals((Class String, HealthCheck.cmp("1 2 +".class)); //write one that checks type
     //}
     @Test
     public void checkIfAddition() {
-        assertEquals(3, HealthCheck.compute("1 2 +"));
+        assertEquals(3, HealthCheck.cmp("1 2 +"));
     }
     @Test
     public void checkIfSubtraction() {
-        assertEquals(-1, HealthCheck.compute("1 2 -"));
+        assertEquals(-1, HealthCheck.cmp("1 2 -"));
     }
     @Test
     public void checkIfMultiplication() {
-        assertEquals(2, HealthCheck.compute("1 2 *"));
+        assertEquals(2, HealthCheck.cmp("1 2 *"));
     }
     @Test
     public void checkIfDivision() {
-        assertEquals(3, HealthCheck.compute("6 2 /"));
+        assertEquals(3, HealthCheck.cmp("6 2 /"));
     }
     @Test
     public void checkIfOtherDivides() {
-        assertEquals(3, HealthCheck.compute("6 2 7"));
+        assertEquals(3, HealthCheck.cmp("6 2 7"));
     }
     @Test
     public void checkIf0returns0() {
-        assertEquals(0, HealthCheck.compute("6 0 /"));
+        assertEquals(0, HealthCheck.cmp("6 0 /"));
+    }
+    @Test(expected = NumberFormatException.class)
+    public void checkBadString() {
+        HealthCheck.cmp("hatt katt fnatt");
     }
 }
